ae871e59a1cd06d47adc94548ba0c2855faf3511
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2022-12-06 19:38:37 +0300

    Add project files.

---
+++ Uyg1TelefonDefteri/DBIslemleri.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Uyg1TelefonDefteri
+{
+    internal class DBIslemleri
+    {
+        static string baglantiYolu = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\source\repos\Programlama II\hafta13\Uyg1TelefonDefteri\Uyg1TelefonDefteri\database\TelefonDefteri.mdf;Integrated Security=True;Connect Timeout=30";
+        //kullanacagimiz veritabanina sag tikladik properties dedik oradan Connection string deki yaziyi tum ekledik
+        static SqlConnection baglanti = new SqlConnection(baglantiYolu);
+        //sql connection veritabanina baglanti acmasi icin
+        public static DataSet UlkeleriCek()
+        {
+            string sql = "Select * from Ulkeler";
+            SqlCommand komutNesnesi = new SqlCommand();//sql tanimlamasi icin komutnesnsi tanimladik
+            komutNesnesi.CommandText = sql;
+            komutNesnesi.Connection = baglanti;
+
+
+            DataSet ulkelerDS = new DataSet();//donen sql komutlari tutmak icin dataset tanimladik
+            SqlDataAdapter adaptor = new SqlDataAdapter();//dataset dogrudan baglanti yapamiyoruz bunu adaptor ile sagladik
+            adaptor.SelectCommand = komutNesnesi; //adaptorun selectCommandina eslestirdik
+
+            baglanti.Open();
+            adaptor.Fill(ulkelerDS); //kayitlari datasete yuklemesine sagladik
+            baglanti.Close();
+
+            return ulkelerDS;
+
+        }
+
+        public static DataSet SehirleriCek(int ulkeID)
+        {
+            string sql = "select *from Sehirler where UlkeID=" + ulkeID;
+            SqlCommand komutNesnesi = new SqlCommand(sql, baglanti);
+
+            DataSet sonuc = new DataSet();
+            SqlDataAdapter adaptor = new SqlDataAdapter(komutNesnesi);
+
+            baglanti.Open();
+            adaptor.Fill(sonuc);
+            baglanti.Close();
+
+            return sonuc;
+
+
+        }
+
+
+        public static void kisiEkle(string a, string s, string t, int sid, string adr)
+        {
+            //insert update delete yapar iken adaptor ve dataset ihtiyac yok
+            // string sql = "insert into Kisiler values ('" + a + "','" + s + "','" + t + "'," + sid + ",'" + adr + "')";
+            string sql = "insert into Kisiler values (@pa,@ps,@pt,@psid,@padr)";
+            SqlCommand komutNesnesi = new SqlCommand(sql, baglanti);
+            komutNesnesi.Parameters.AddWithValue("@pa", a);
+            komutNesnesi.Parameters.AddWithValue("@ps", s);
+            komutNesnesi.Parameters.AddWithValue("@pt", t);
+            komutNesnesi.Parameters.AddWithValue("@psid", sid);
+            komutNesnesi.Parameters.AddWithValue("@padr", adr);
+
+            baglanti.Open();
+            komutNesnesi.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        public static DataSet Arama(string ad)
+        {
+            string sql = "select * from Kisiler where Adi like @pa+'%'";// sonu ne olursa olsun % ifadesi
+            SqlCommand komutNesnesi = new SqlCommand(sql, baglanti);
+            komutNesnesi.Parameters.AddWithValue("@pa", ad);
+            SqlDataAdapter adaptor = new SqlDataAdapter(komutNesnesi);
+            DataSet sonuc = new DataSet();
+            baglanti.Open();
+            adaptor.Fill(sonuc);
+            baglanti.Close();
+            return sonuc;
+        }
+
+        public static void Guncelle(string a, string s, string t, string adr, int kid)
+        {
+            string sql = "Update Kisiler set Adi=@pa,Asoyadi=@ps,Adres=@padr, Telefon=@pt where KisiID=@pkid";
+            SqlCommand komutNesnesi = new SqlCommand(sql, baglanti);
+            komutNesnesi.Parameters.AddWithValue("@pa", a);
+            komutNesnesi.Parameters.AddWithValue("@ps", s);
+            komutNesnesi.Parameters.AddWithValue("@padr", adr);
+            komutNesnesi.Parameters.AddWithValue("@pt", t);
+            komutNesnesi.Parameters.AddWithValue("@pkid", kid);
+
+            baglanti.Open();
+            komutNesnesi.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        public static void Sil(int kid)
+        {
+            string sql = "delete from Kisiler where KisiID=" + kid;
+            SqlCommand komutNesnesi = new SqlCommand(sql, baglanti);
+
+            baglanti.Open();
+            komutNesnesi.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+    }
+}
---
+++ Uyg1TelefonDefteri/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uyg1TelefonDefteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataSet ulkeler = DBIslemleri.UlkeleriCek();
+            comboBox1.DisplayMember = "UlkeAdi"; // bize gosterecegi kisim
+            comboBox1.ValueMember = "UlkeID";// degerini alacagi kisim
+            comboBox1.DataSource = ulkeler.Tables[0];//ulkeler ismi tables [0] tabloyu attik
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ulkeID = (int)comboBox1.SelectedValue;
+            DataSet sehirlerDS=DBIslemleri.SehirleriCek(ulkeID);
+            comboBox2.DisplayMember = "Sehir";
+            comboBox2.ValueMember = "SehirID";
+            comboBox2.DataSource = sehirlerDS.Tables[0];
+
+
+            
+                /*DataSet sehirler = DBIslemleri.SehirleriCek();
+            comboBox1.DisplayMember = "SehirAdi";
+            comboBox1.ValueMember = "SehirID";
+            comboBox1.DataSource = sehirler.Tables[0];*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adi = textBox1.Text;
+            string soyadi=textBox2.Text;
+            string telefon = textBox3.Text;
+            int sid = (int)comboBox2.SelectedValue;
+            string adres = textBox4.Text;
+
+            DBIslemleri.kisiEkle(adi, soyadi, telefon, sid, adres);
+            MessageBox.Show("Kisi Eklendi");
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            DataSet sonuclar = DBIslemleri.Arama(textBox5.Text);
+            dataGridView1.DataSource = sonuclar.Tables[0];
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedRows.Count > 0)//secili satir var mi?
+            {
+                int kid = (int)dataGridView1.SelectedRows[0].Cells[0].Value; // cell 0 hucre
+                textBox8.Text= dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                textBox7.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                textBox9.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+                textBox6.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)//secili satir var mi?
+            {
+                int kid = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+                string yeniAd = textBox8.Text;
+                string yeniSoyad = textBox7.Text;
+                string yeniTelefon = textBox9.Text;
+                string yeniAdres = textBox6.Text;
+
+                DBIslemleri.Guncelle(yeniAd, yeniSoyad, yeniTelefon, yeniAdres,kid);
+                MessageBox.Show("Guncellendi");
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)//secili satir var mi?
+            {
+                int kid = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+
+                DBIslemleri.Sil(kid);
+                MessageBox.Show("Silindi");
+
+            }
+
+        }
+    }
+}
